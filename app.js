0b28bc550e81aa64cd866fcfd048439d2e1138cc
Julie Knox <julie.knox.cohort.225@gmail.com>
2023-01-13 12:13:30 -0800

    some progress on the keystroke comparing

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const quote = document.getElementById('test').innerText;
+
+for(let i = 0; i < quote.length; i++) {
+    //Event listener for when a key is pressed
+    document.addEventListener('keyup', 
+    function(e) {
+        //Compare if the correct key is pressed
+        if(e.key === quote[i]) {
+           console.log('correct');
+        } else {
+           console.log('incorrect');
+        }
+        e.preventDefault();
+    });
+}
