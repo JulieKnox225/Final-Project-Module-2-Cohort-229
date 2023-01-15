8cb2950501db961101f6e7d19bddcb33277af358
Julie Knox <julie.knox.cohort.225@gmail.com>
2023-01-14 18:08:35 -0800

    converted to ES6 class

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
-//Collection of possible quotes
-const quotes = [
-    'The quick brown fox jumps over the lazy dog',
-    'Jaded zombies acted quaintly but kept driving their oxen forward',
-    'My girl wove siz dozen plaid jackets before she quit',
-    'The key to success is failure',
-    'If there is no struggle, there is no progress',
-    'The early bird catches the worm',
-    "Don't let yesterday take up too much of today",
-    'Wherever you go, go with all your heart',
-    "Believe you can and you're halfway there",
-    'Not all who wonder are lost',
-    'Change is inevitable. Growth is optional',
-    'Oh yes, the past can hurt. But you can either run from it, or learn from it',
-    'You sit on a throne of lies',
-    'Happiness can be found even in the darkest of times, if one only remembers to turn on the light',
-    "I'm gonna make him an offer he can't refuse",
-    "Life is like a box of chocolates, you never know what you're gonna get"
-];
-
-//Function to pick the new quote
-function pickQuote(current) {
-    let newQuote = quotes[Math.floor(Math.random() * quotes.length)];
-
-    while(newQuote == current) {
-        newQuote = quotes[Math.floor(Math.random() * quotes.length)];
-        console.log('same');
+class Quotes {
+    //Collection of possible quotes
+    constructor() {
+        this.quoteCollection = [
+            'The quick brown fox jumps over the lazy dog',
+            'Jaded zombies acted quaintly but kept driving their oxen forward',
+            'My girl wove siz dozen plaid jackets before she quit',
+            'The key to success is failure',
+            'If there is no struggle, there is no progress',
+            'The early bird catches the worm',
+            "Don't let yesterday take up too much of today",
+            'Wherever you go, go with all your heart',
+            "Believe you can and you're halfway there",
+            'Not all who wonder are lost',
+            'Change is inevitable. Growth is optional',
+            'Oh yes, the past can hurt. But you can either run from it, or learn from it',
+            'You sit on a throne of lies',
+            'Happiness can be found even in the darkest of times, if one only remembers to turn on the light',
+            "I'm gonna make him an offer he can't refuse",
+            "Life is like a box of chocolates, you never know what you're gonna get"
+        ];
     }
-
-    console.log('different');
-    return newQuote;
+    
+    //Method to pick the new quote
+    pickQuote(current = this.quoteCollection[0]) {
+        let newQuote = this.quoteCollection[Math.floor(Math.random() * this.quoteCollection.length)];
+    
+        while(newQuote == current) {
+            newQuote = this.quoteCollection[Math.floor(Math.random() * this.quoteCollection.length)];
+            console.log('same');
+        }
+    
+        console.log('different');
+        return newQuote;
+    }
+        
+    //Method to display current quote
 }
 
+
+
+
 //Pick a current quote
-let quote = pickQuote(0);
+let quote = new Quotes;
+quote.pickQuote();
 
 //To iterate over the quote
 let i = 0; 
@@ -40,6 +50,7 @@ let i = 0;
 //Event listener for when a key is pressed
 document.addEventListener('keyup', 
     function(e) {
+        console.log(e.target);
         //Compare if the correct key is pressed
         if(e.key === quote[i]) {
            console.log('correct');
